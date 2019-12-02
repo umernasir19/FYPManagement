03c92b806c4f2ad933a5e40a3f32b078d4bc3c96
umernasir19 <umernasir19@gmail.com>
2019-12-02 08:10:25 +0500

    first commit

---
+++ FYPAUtOMATION/FYPAUtOMATION/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FYPAUtOMATION.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public JsonResult Login(User users)
+        {
+            return Json(new { users = users }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
